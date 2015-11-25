8edaa9a266dc652d7ba30142898e5ba0d4a5eb78
givemin5 <givemin5@hotmail.com>
2015-11-25 23:51:16 +0800

    策略模式範例

---
+++ StrategyPattern/Airplane.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPattern
+{
+    abstract class Airplane
+    {
+        public abstract void Display();
+
+        public void Fly()
+        {
+            Console.WriteLine("我會飛");
+        }
+
+        public abstract void Shoot();
+
+
+    }
+}
---
+++ StrategyPattern/Fighter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPattern
+{
+    class Fighter : Airplane
+    {
+        public override void Display()
+        {
+            Console.Write("我是戰鬥機");
+        }
+
+        public override void Shoot()
+        {
+            Console.Write("我有槍");
+        }
+    }
+}
