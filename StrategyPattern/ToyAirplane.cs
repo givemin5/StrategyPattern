f6c9b41ffe6ae606cc5bbf0cbe7875b98e7bbd52
givemin5 <givemin5@hotmail.com>
2015-11-26 00:36:34 +0800

    新增 玩具飛機

---
+++ StrategyPattern/ToyAirplane.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPattern
+{
+    class ToyAirplane : Airplane
+    {
+        public ToyAirplane()
+        {
+            _fly = new NoFly();
+            _shoot = new NoShoot();
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine("我是玩具飛機");
+        }
+    }
+}
